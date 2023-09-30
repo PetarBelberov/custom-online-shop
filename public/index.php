cf8729f97ea87927c87ab2bd9e6c4ad1ee96a580
PetarBelberov <petar_belberov@gmx.com>
2023-09-30 23:20:45 +0300

    Add '/'.

--- public/index.php
+++ public/index.php
@@ -2,9 +2,9 @@
 
 //require_once __DIR__ . '/../autoload.php';
 require __DIR__ . '/vendor/autoload.php';
-require_once __DIR__ . 'config/database.php';
-require_once __DIR__ . 'src/Controllers/UserController.php';
-require_once __DIR__ . 'routes/web.php';
+require_once __DIR__ . '/config/database.php';
+require_once __DIR__ . '/src/Controllers/UserController.php';
+require_once __DIR__ . '/routes/web.php';
 
 try {
     // Handle the request based on the URL.
